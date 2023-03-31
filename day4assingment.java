38947de988787579ffbcb3a4eee7a1ba93c77420
Sujay239 <125623523+Sujay239@users.noreply.github.com>
2023-03-31 18:59:53 +0530

    Add files via upload

---
+++ day4assingment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class day4assingment{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+System.out .print("enter a :");
+int a = sc.nextInt();
+
+System.out.print("enter b :");
+int b = sc.nextInt();
+
+ int sum = a+b;
+
+ System.out.println("The sum is :" + sum);
+
+}
+}
